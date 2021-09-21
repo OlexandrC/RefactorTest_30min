d21e8adcac3af763ab01ea790f98f3496b7b4137
OlexandrC <tsfp.space@gmail.com>
2021-09-21 09:08:11 +0300

    First commit

---
+++ ConsoleApp1/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refactoring
+{
+    public class Product
+    {
+        public string ProductName;
+        public decimal Price;
+        public decimal? Weight;
+        public int? Quantity;
+        public EnumPricingMethod PricingMethod;
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Refactoring
+{
+    class Program
+    {
+        static decimal price;
+        static string orderSummary;
+        static Tuple<string, List<Product>> order;
+
+        static void Main(string[] args)
+        {
+            CreateInputData();
+
+            MakeCalculation();
+
+            PrintResult();
+
+        }
+
+        static void CreateInputData()
+        {
+            order = new Tuple<string, List<Product>>("John Doe",
+                new List<Product>
+                {
+                new Product
+                {
+                    ProductName = "Pulled Pork",
+                    Price = 6.99m,
+                    Weight = 0.5m,
+                    PricingMethod = EnumPricingMethod.PerPound,
+                },
+                new Product
+                {
+                    ProductName = "Coke",
+                    Price = 3m,
+                    Quantity = 2,
+                    PricingMethod = EnumPricingMethod.PerItem
+                }
+            }
+            ); ;
+            price = 0m;
+            orderSummary = "ORDER SUMMARY FOR " + order.Item1 + ": \r\n";
+        }
+
+        static void MakeCalculation()
+        {
+            foreach (var orderProduct in order.Item2)
+            {
+                orderSummary += orderProduct.ProductName;
+
+                CalculateProduct(orderProduct);
+
+                orderSummary += "\r\n";
+            }
+        }
+
+        static void PrintResult()
+        {
+            Console.WriteLine(orderSummary);
+            Console.WriteLine("Total Price: $" + price);
+
+            Console.ReadKey();
+        }
+
+        static void CalculateProduct(Product orderProduct)
+        {
+            var productPrice = 0m;
+
+            if (orderProduct.PricingMethod == EnumPricingMethod.PerPound)
+            {
+                productPrice = (orderProduct.Weight.Value * orderProduct.Price);
+            }
+            else if (orderProduct.PricingMethod == EnumPricingMethod.PerItem)
+            {
+                productPrice = (orderProduct.Quantity.Value * orderProduct.Price);
+            }
+            else
+            {
+                throw new Exception("Unexpected price method");
+            }
+
+            price += productPrice;
+            orderSummary += string.Format(" ${0} ({1} pounds at ${2} per pound)", productPrice, orderProduct.Weight, orderProduct.Price);
+        }
+    }
+
+}
+
